d8ac343649d85c9c2ddb130bc495cb8b86106198
Dennis Yurichev <dennis@yurichev.com>
2013-05-17 13:38:54 +0300

    =...

--- update_tags.bat
+++ update_tags.bat
@@ -1,5 +1,5 @@
-c:\s\ctags58\ctags *.c *.h
-c:\s\ctags58\ctags -a ..\octothorpe\*.c ..\octothorpe\*.h
-c:\s\ctags58\ctags -a ..\bolt\*.c ..\bolt\*.h
-c:\s\ctags58\ctags -a ..\x86_disasm\*.c ..\x86_disasm\*.h
-c:\s\ctags58\ctags -a ..\porg\*.c ..\porg\*.h
+ctags *.c *.h
+ctags -a ../octothorpe/*.c ../octothorpe/*.h
+ctags -a ../bolt/*.c ../bolt/*.h
+ctags -a ../x86_disasm/*.c ../x86_disasm/*.h
+ctags -a ../porg/*.c ../porg/*.h
