10d7c9ca37ad9ad34cddce781e2177e7e8046677
Dennis Yurichev <dennis@yurichev.com>
2013-07-01 02:22:02 +0300

    =...

--- tests/test1_BPF.c
+++ tests/test1_BPF.c
@@ -1,3 +1,18 @@
+/*
+ *  _______                      
+ * |__   __|                     
+ *    | |_ __ __ _  ___ ___ _ __ 
+ *    | | '__/ _` |/ __/ _ \ '__|
+ *    | | | | (_| | (_|  __/ |   
+ *    |_|_|  \__,_|\___\___|_|   
+ *
+ * Written by Dennis Yurichev <dennis(a)yurichev.com>, 2013
+ *
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License. 
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/.
+ *
+ */
+
 #include <stdio.h>
 #include <windows.h>
 
@@ -7,6 +22,22 @@ void change_some_byte (char *buf, int i)
     buf[i+1]='a';
     buf[i+2]='h';
     buf[i+3]='a';
+};
+
+double fac(unsigned a)
+{
+    if (a==0)
+        return 0.0;
+
+    if (a==1)
+        return 1.0;
+    
+    return fac(a-1) * (double)a;
+};
+
+void FPU_test()
+{
+    printf ("fac(5)=%f\n", fac(5));
 };
 
 int main()
@@ -25,4 +56,6 @@ int main()
     char *buf2=strdup("The quick brown fox jumps over the lazy dog");
 //    char *buf2="The quick brown fox jumps over the lazy dog";
     change_some_byte(buf2, 3);
+
+    FPU_test();
 };
