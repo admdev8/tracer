4a07555980c8362e2bcfe6642816d140a9fc3966
Dennis Yurichev <dennis@yurichev.com>
2013-06-30 19:36:54 +0300

    =copyright/licensing information added

--- process.c
+++ process.c
@@ -1,3 +1,18 @@
+/*
+ *  _______                      
+ * |__   __|                     
+ *    | |_ __ __ _  ___ ___ _ __ 
+ *    | | '__/ _` |/ __/ _ \ '__|
+ *    | | | | (_| | (_|  __/ |   
+ *    |_|_|  \__,_|\___\___|_|   
+ *
+ * Written by Dennis Yurichev <dennis(a)yurichev.com>, 2013
+ *
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License. 
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/.
+ *
+ */
+
 #include <assert.h>
 
 #include "process.h"
